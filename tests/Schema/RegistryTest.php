d62e11a57fdcd7beae908eb74806eeeb2095ad25
Wolfy-J <wolfy.jd@gmail.com>
2019-03-26 16:00:11 +0300

    child tests

--- tests/Schema/RegistryTest.php
+++ tests/Schema/RegistryTest.php
@@ -8,8 +8,12 @@
 
 namespace Cycle\Schema\Tests;
 
+use Cycle\ORM\Schema;
+use Cycle\Schema\Compiler;
 use Cycle\Schema\Definition\Entity;
+use Cycle\Schema\Definition\Field;
 use Cycle\Schema\Registry;
+use Cycle\Schema\Tests\Fixtures\Author;
 use Cycle\Schema\Tests\Fixtures\Post;
 use Cycle\Schema\Tests\Fixtures\User;
 
@@ -100,5 +104,74 @@ abstract class RegistryTest extends BaseTest
         $r = new Registry($this->dbal);
 
         $r->getTableSchema(new Entity());
+    }
+
+    /**
+     * @expectedException \Cycle\Schema\Exception\RegistryException
+     */
+    public function testRegisterChildNoEntity()
+    {
+        $e = new Entity();
+        $e->setRole('parent');
+        $e->setClass(Author::class);
+
+        $e->getFields()->set(
+            'id',
+            (new Field())->setType('primary')->setColumn('id')
+        );
+
+        $r = new Registry($this->dbal);
+
+        $c = new Entity();
+        $c->setRole('parent');
+        $c->setClass(User::class);
+
+        $c->getFields()->set(
+            'id',
+            (new Field())->setType('primary')->setColumn('id')
+        );
+
+        $c->getFields()->set(
+            'name',
+            (new Field())->setType('string')->setColumn('name')
+        );
+
+        $r->registerChild($e, $c);
+    }
+
+    public function testRegisterChild()
+    {
+        $e = new Entity();
+        $e->setRole('parent');
+        $e->setClass(Author::class);
+
+        $e->getFields()->set(
+            'id',
+            (new Field())->setType('primary')->setColumn('id')
+        );
+
+        $r = new Registry($this->dbal);
+        $r->register($e)->linkTable($e, 'default', 'table');
+
+        $c = new Entity();
+        $c->setRole('parent');
+        $c->setClass(User::class);
+
+        $c->getFields()->set(
+            'id',
+            (new Field())->setType('primary')->setColumn('id')
+        );
+
+        $c->getFields()->set(
+            'name',
+            (new Field())->setType('string')->setColumn('name')
+        );
+
+        $r->registerChild($e, $c);
+        $this->assertTrue($e->getFields()->has('name'));
+
+        $schema = (new Compiler())->compile($r, []);
+
+        $this->assertSame('parent', $schema[User::class][Schema::ROLE]);
     }
 }
