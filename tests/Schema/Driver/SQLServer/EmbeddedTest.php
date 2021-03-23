57403d70bb0841b1ab87196946ea3b53faaf8d88
roxblnfk <roxblnfk@ya.ru>
2021-03-23 18:47:02 +0300

    Fix Embedded tests (added for Drivers)

---
+++ tests/Schema/Driver/SQLServer/EmbeddedTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Schema\Tests\Driver\SQLServer;
+
+class EmbeddedTest extends \Cycle\Schema\Tests\Relation\EmbeddedTest
+{
+    public const DRIVER = 'sqlserver';
+}
