55b7c25d54284d0a6fe1ac8c8eda115d7ad18b80
butschster <butschster@gmail.com>
2021-11-16 20:27:31 +0300

    Fixes tests

--- tests/Schema/Fixtures/BrokenSchemaModifier.php
+++ tests/Schema/Fixtures/BrokenSchemaModifier.php
@@ -4,28 +4,48 @@ declare(strict_types=1);
 
 namespace Cycle\Schema\Tests\Fixtures;
 
+use Cycle\Schema\Exception\SchemaModifierException;
 use Cycle\Schema\Registry;
 use Cycle\Schema\SchemaModifierInterface;
 
 class BrokenSchemaModifier implements SchemaModifierInterface
 {
+    public function __construct(private string $brokenMethod)
+    {
+    }
+
     public function withRole(string $role): static
     {
-        // TODO: Implement withRole() method.
+        if ($this->brokenMethod === __METHOD__) {
+            $this->throwException();
+        }
+
+        return $this;
     }
 
     public function compute(Registry $registry): void
     {
-        // TODO: Implement compute() method.
+        if ($this->brokenMethod === __METHOD__) {
+            $this->throwException();
+        }
     }
 
     public function render(Registry $registry): void
     {
-        // TODO: Implement render() method.
+        if ($this->brokenMethod === __METHOD__) {
+            $this->throwException();
+        }
     }
 
     public function modifySchema(array &$schema): void
     {
-        throw new \Exception('Something went wrong');
+        if ($this->brokenMethod === __METHOD__) {
+            $this->throwException();
+        }
+    }
+
+    private function throwException(): void
+    {
+        throw new SchemaModifierException('Something went wrong');
     }
 }
