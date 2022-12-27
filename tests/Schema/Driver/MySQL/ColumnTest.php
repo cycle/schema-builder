4d7d2440c1533ee971d1758c4897948e55267990
roxblnfk <roxblnfk@ya.ru>
2022-12-27 22:46:22 +0300

    Add test

--- tests/Schema/Driver/MySQL/ColumnTest.php
+++ tests/Schema/Driver/MySQL/ColumnTest.php
@@ -4,9 +4,32 @@ declare(strict_types=1);
 
 namespace Cycle\Schema\Tests\Driver\MySQL;
 
+use Cycle\Schema\Definition\Field;
+use Cycle\Schema\Table\Column;
 use Cycle\Schema\Tests\ColumnTest as BaseTest;
 
 class ColumnTest extends BaseTest
 {
     public const DRIVER = 'mysql';
+
+    public function testAttributesUnsigned(): void
+    {
+        $field = new Field();
+        $field->setType('integer');
+        $field->setColumn('name');
+        $field->getAttributes()->set('unsigned', true);
+
+        $table = $this->getStub();
+        $column = Column::parse($field);
+
+        $column->render($table->column('name'));
+
+        $table->save();
+
+        $table = $this->getStub();
+        $this->assertTrue($table->hasColumn('name'));
+        $this->assertArrayHasKey('unsigned', $table->column('name')->getAttributes());
+        $this->assertTrue($table->column('name')->getAttributes()['unsigned']);
+        $this->assertTrue($table->column('name')->isUnsigned());
+    }
 }
